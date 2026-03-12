c629295733b2e3e090bf37bcbdbcae9cb1ba530d
Quint Daenen <quint@daenen.email>
2026-03-12 10:03:42 +0100

    fix: link libc explicitly for cross-platform builds

--- build.zig
+++ build.zig
@@ -9,6 +9,7 @@ pub fn build(b: *std.Build) void {
         .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
+        .link_libc = true,
     });
     linkPcsc(mod, b);
 
